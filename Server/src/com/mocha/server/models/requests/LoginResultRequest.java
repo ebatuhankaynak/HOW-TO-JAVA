aaa230831e0d2bc6ad653fdb247376a82640bb3f
ebatuhankaynak <ebatuhankaynak@gmail.com>
2016-04-25 00:44:36 +0300

    sa

--- Server/src/com/mocha/server/models/requests/LoginResultRequest.java
+++ Server/src/com/mocha/server/models/requests/LoginResultRequest.java
@@ -1,14 +1,16 @@
 package com.mocha.server.models.requests;
 
+import com.mocha.server.models.User;
 import com.mocha.server.models.results.LoginResults;
 
 public class LoginResultRequest {
 
     private LoginResults result;
+    private User user;
 
-
-    public LoginResultRequest(LoginResults result) {
+    public LoginResultRequest(LoginResults result, User user) {
         this.result = result;
+        this.user = user;
     }
 
     public LoginResults getResult() {
@@ -17,5 +19,13 @@ public class LoginResultRequest {
 
     public void setResult(LoginResults result) {
         this.result = result;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
